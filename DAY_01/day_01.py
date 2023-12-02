f40aa8091999bb9acba8fa46cac41154ecd0d64b
Leonard Quinones <leon0327@protonmail.ch>
2023-12-01 20:12:54 -0500

    create find_calibration_values and add support for numbers in letters

--- DAY_01/day_01.py
+++ DAY_01/day_01.py
@@ -3,35 +3,67 @@
 """
 Created on Fri Dec  1 15:32:33 2023
 
-@author: leonard
+@author: Leonard Quinones
 """
+    
+from typing import Union, Tuple, List
 
-from typing import Union, Tuple
+NUMBERS_ALP_NUM = {'one': '1',
+                   'two': '2', 
+                   'three': '3', 
+                   'four': '4', 
+                   'five': '5', 
+                   'six': '6', 
+                   'seven': '7', 
+                   'eight': '8', 
+                   'nine': '9',
+                   'zero': '0'
+                   }
 
-def find_indexes(string: str) -> Tuple[int, int]:
-    indexes: list[int] = list(filter(lambda x: x if x.isnumeric() else False, string))
-    number_of_digits = len(indexes)
-    if number_of_digits:
-        if number_of_digits == 1:
-            return (indexes[0], indexes[0])
-        if number_of_digits >= 2:
-            return (indexes[0], indexes[-1])
+def find_numbers(string: str, numeric_symbols: List[str]) -> List[Tuple] :
+    found_numbers = []
+    for number in numeric_symbols:
+        found_index = string.find(number)
+        found_rindex = string.rfind(number)
+        if found_index > -1:
+            found_numbers.append((found_index, number))
+        if found_rindex > -1:
+            found_numbers.append((found_rindex, number))
+    found_numbers = sorted(found_numbers)
+    if found_numbers:
+        if len(found_numbers) == 1:
+            return (found_numbers[0], found_numbers[0])
+        if len(found_numbers) >= 2:
+            return (found_numbers[0], found_numbers[-1])
     return None
 
-def build_number(indexes: iter):
-    return int(''.join(indexes))
+def find_calibration_values(string: str) -> Union[Tuple,None]:
+    alpha_numbers =  find_numbers(string, numeric_symbols=NUMBERS_ALP_NUM.keys())
+    if alpha_numbers:
+        alpha_numbers = tuple(map(
+            lambda x: (x[0], NUMBERS_ALP_NUM.get(x[1])) if x[1] in NUMBERS_ALP_NUM.keys() 
+                       else x, alpha_numbers))
+    digits =  find_numbers(string, numeric_symbols=NUMBERS_ALP_NUM.values())
+    if (alpha_numbers and digits):
+        found_numbers = sorted(alpha_numbers + digits)
+        return (found_numbers[0][1], found_numbers[-1][1])
+    
+    if alpha_numbers and not digits:
+        return (alpha_numbers[0][1], alpha_numbers[-1][1])
+
+    if not alpha_numbers and digits:
+        return (digits[0][1], digits[-1][1])    
+    
+    return None
 
 
-"""
-print(find_indexes('1abc2'))
-print(find_indexes('pqr3stu8vwx'))
-print(find_indexes('a1b2c3d4e5f'))
-print(find_indexes('treb7uchet'))
-"""
+def build_number(numbers_symbols: iter):
+    return int(''.join(numbers_symbols))
 
-with open('/home/leonard/Documents/AOC2023/DAY_01/input_day01', 'r') as f:
+
+with open('/home/leonard/Documents/AOC2023/DAY_01/input_day01_half_two', 'r') as f:
     found_digits: list[int] = [] 
     for line in f:
-        found_digits.append(build_number(find_indexes(line)))
+        found_digits.append(build_number(find_calibration_values(line)))
     print(sum(found_digits))
     

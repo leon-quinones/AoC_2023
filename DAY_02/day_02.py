d87b9c61401ab2e29ac90442d1482f086ae44607
Leonard Quinones <leon0327@protonmail.ch>
2023-12-02 14:45:39 -0500

    create solution for day 2 half two

--- DAY_02/day_02.py
+++ DAY_02/day_02.py
@@ -1,14 +1,24 @@
 from functools import reduce
 from typing import Union, Tuple, List, Dict, NamedTuple
 from collections import namedtuple
-def check_sets_validity(game_sets: List[str], game_settings: Dict) -> bool:
+
+def check_game_validity(game: str, game_settings: Dict, game_delimiters: NamedTuple) -> Tuple:
+    game_id, cubes_sets = get_game_info(game, game_delimiters)
+    is_valid_game, power = check_sets_validity(cubes_sets, settings)
+    return game_id, is_valid_game, power
+
+
+def check_sets_validity(game_sets: List[str], game_settings: Dict) -> Tuple[bool, int]:
+    power = 1
     parameters = list(game_settings.keys())
     for parameter in parameters:
         max_number = game_settings.get(parameter)
         cubes_by_parameter = get_cubes_by_parameter(game_sets, parameter)
         if max(cubes_by_parameter) > max_number:
-            return False
-    return True
+            return (False, 0)
+        power *= max(cubes_by_parameter)
+    return True, power
+
 
 def get_cubes_by_parameter(game_sets: List[str], parameter: str) -> List[int]:
     gathered_cubes = []
@@ -33,18 +43,16 @@ def get_game_info(game: str, delimiters: NamedTuple) -> Tuple[str, List[str]]:
 
 
 
-settings = {'red': 12, 'blue': 14, 'green': 13}
+settings = {'red': 120, 'blue': 140, 'green': 130}
 Delimiters = namedtuple('Game_Delimiter', 'game_delimiter game_name '
                                           'cubes_set_delimiter cube_delimiter')
 game_delimiters = Delimiters(':', 'Game', ';', ',')
-
-def check_game_validity(game: str, game_settings: Dict, game_delimiters: NamedTuple) -> Tuple:
-    game_id, cubes_sets = get_game_info(game, game_delimiters)
-    is_valid_game = check_sets_validity(cubes_sets, settings)
-    return game_id, is_valid_game
-
-with open('./day_02_input', 'r') as f:
+test_line = 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red'
+print(check_game_validity(test_line, settings, game_delimiters))
+print("###")
+with open('./day_02_input_2', 'r') as f:
     valid_games = []
     for line in f:
         valid_games.append(check_game_validity(line, settings, game_delimiters))
     print(sum([x[0] for x in valid_games if x[1]]))
+    print(sum([x[2] for x in valid_games if x[1]]))
